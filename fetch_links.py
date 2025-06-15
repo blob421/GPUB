96a2bd1562fb89421e89652e4f9d8c994bc8cd2d
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2025-06-15 19:14:11 -0400

    1-2 seconds faster by integrating asyncio and aiosqlite to fetch_links

--- fetch_links.py
+++ fetch_links.py
@@ -1,24 +1,28 @@
-import requests
 import re
 from bs4 import BeautifulSoup
 import sqlite3
 import yaml
+import asyncio
+import aiohttp
 #import cProfile
+import aiosqlite
 
 # Input part 
+
+DB_PATH = 'site.sqlite'
 
 def fetch():
  try:
    with open("parameters.txt", "r") as params:
       dictio = yaml.safe_load(params.read())
-      n = dictio["n"]
+      num = dictio["n"]
       item = dictio["item"]
 
  except FileNotFoundError:
-    n = input('How many pages to search (0-10) ? 3 is recommended : ')
+    num = input('How many pages to search (0-10) ? 3 is recommended : ')
     item = input('What are you looking for ? (6700 xt , tuf 5080 ti, 4070 ti super, astral) : ')
     list = dict()
-    list["n"] = n
+    list["n"] = num
     list["item"] = item
     
     with open("parameters.txt", "w") as file:
@@ -53,15 +57,13 @@ def fetch():
 
  ## Function to add desired links to the database
 
- def add_to_db():
-  
-  page = 1
-  while page <= int(n):             #Retreives n pages
- 
-       with requests.get(f'https://www.newegg.ca/p/pl?N=100006663&page={page}') as response:
-      
+ async def add_to_db(n):
+                                #Retreives n pages
+  async with aiohttp.ClientSession() as requests:
+    async with requests.get(f'https://www.newegg.ca/p/pl?N=100006663&page={n}') as response:
+        response_text = await response.text()
     
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response_text, 'html.parser')
         anchors = soup('a')
     
         for anchor in anchors:
@@ -81,9 +83,9 @@ def fetch():
                 
                 if not '#IsFeed' in href:
                  if not '?Item=' in href:
-                   
-                    cursor.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
-           
+                    async with aiosqlite.connect(DB_PATH) as db:
+                     await db.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
+                     await db.commit()    
            elif item1:
              if re.search(item0.lower(), href.lower()) and re.search(item1.lower(), href.lower()):
                
@@ -96,9 +98,12 @@ def fetch():
                 if not '#IsFeed' in href:
                  if not '?Item=' in href:
                    
-                    cursor.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
+                   async with aiosqlite.connect(DB_PATH) as db:
+                     await db.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
+                     await db.commit()    
            
            else:
+            
              if re.search(item.lower(), href.lower()):
              
               if not "ti" in item.lower() and "ti" in href.lower():
@@ -111,21 +116,22 @@ def fetch():
               if not '#IsFeed' in href:
                  if not '?Item=' in href:
           
-                   cursor.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
-        conn.commit()    #Add links to the db 
-
-      
-        page += 1
- 
-  cursor.close()
-  conn.close()
+                   async with aiosqlite.connect(DB_PATH) as db:
+                     await db.execute('INSERT OR IGNORE INTO links (links) VALUES ( ? )', ( (href), ) )
+                     await db.commit()    
   
- add_to_db() # was 21.24s now 2.34s
 
+ 
+
+ async def main():
+
+   tasks = [add_to_db(numb) for numb in range(int(num))]
+   await asyncio.gather(*tasks)
+    
+ asyncio.run(main())
+ 
 
 #cProfile.run("fetch()")
-
-
 
 
  
